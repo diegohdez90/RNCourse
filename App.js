1a87def4c7ed25d60c0da0f9c077efeda6e82ceb
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-11-27 09:25:53 -0600

    feat: add style to input container

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View
+        style={styles.inputContainer}
+      >
         <TextInput
+          style={styles.textInput}
           placeholder='Type your goal'
         />
         <Button title='Add goal' />
@@ -20,5 +23,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderColor: '#666',
+    borderRadius: 8,
+    borderWidth: 2,
+    width: '80%',
+    marginEnd: 8,
+    padding: 4
   }
 });
